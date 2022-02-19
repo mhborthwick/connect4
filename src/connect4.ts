7006df82d79129e892066a04e745635339938e93
mhborthwick <mhborthwick@gmail.com>
2022-02-18 20:51:18 -0800

    chore: set up skeleton

--- src/connect4.ts
+++ src/connect4.ts
@@ -1,1 +1,21 @@
-console.log("hi");
+interface Display {}
+
+// -- Display --
+class DOMDisplay {
+  // DOMDisplay class
+}
+
+// -- Model --
+class Connect4 {
+  display: Display;
+  constructor(display: Display) {
+    this.display = display;
+  }
+  startGame() {
+    return;
+  }
+}
+
+// -- Start Game --
+const connect4 = new Connect4(new DOMDisplay());
+connect4.startGame();
