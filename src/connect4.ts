7ed467c3d8f44bb8a3a9b2b528ebb4285629a924
mhborthwick <mhborthwick@gmail.com>
2022-02-19 14:42:42 -0800

    chore: start displaying score on browser

--- src/connect4.ts
+++ src/connect4.ts
@@ -1,4 +1,6 @@
 interface Display {
+  getElement(selector: string): HTMLElement;
+  createElement(selector: string, className: string): HTMLElement;
   printScoreBoard(score: Score): void;
 }
 
@@ -8,9 +10,28 @@ interface Score {
 }
 
 // -- Display --
-class DOMDisplay {
+class DOMDisplay implements Display {
+  getElement(selector: string): HTMLElement {
+    return <HTMLElement>document.querySelector(selector);
+  }
+
+  createElement(tag: string, className?: string): HTMLElement {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
+
   printScoreBoard(score: Score): void {
-    console.log(score);
+    const game = this.getElement("#game");
+    const scoreBoard = this.createElement("div", "score");
+    game.append(scoreBoard);
+    const playerOneScore = this.createElement("div", "red");
+    playerOneScore.textContent = `Player 1: ${score.red}`;
+    playerOneScore.id = "score-red";
+    const playerTwoScore = this.createElement("div", "blue");
+    playerTwoScore.textContent = `Player 2: ${score.blue}`;
+    playerTwoScore.id = "score-blue";
+    scoreBoard.append(playerOneScore, playerTwoScore);
   }
 }
 
