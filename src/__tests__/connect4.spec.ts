867a259ee52be7c9e78ae92b549fddbcfb94114e
mhborthwick <mhborthwick@gmail.com>
2022-02-20 01:14:09 -0800

    test: add unit tests

--- src/__tests__/connect4.spec.ts
+++ src/__tests__/connect4.spec.ts
@@ -1,4 +1,30 @@
-it("Dummy unit test", () => {
-  const test = 1;
-  expect(test).toBe(1);
+import Connect4 from "../classes/connect4";
+import DOMDisplay from "../classes/domDisplay";
+
+describe("Class: Connect4", () => {
+  describe("Method: startGame", () => {
+    let domDisplay: DOMDisplay;
+    let connect4: Connect4;
+
+    beforeEach(() => {
+      jest
+        .spyOn(DOMDisplay.prototype, "printScoreBoard")
+        .mockImplementationOnce(jest.fn());
+      domDisplay = new DOMDisplay();
+      connect4 = new Connect4(domDisplay);
+    });
+
+    it("should print score board once when game starts", () => {
+      connect4.startGame();
+      expect(domDisplay.printScoreBoard).toHaveBeenCalledTimes(1);
+    });
+
+    it("should print score board with { red: 0, blue: 0 } when game starts", () => {
+      connect4.startGame();
+      expect(domDisplay.printScoreBoard).toHaveBeenCalledWith({
+        red: 0,
+        blue: 0,
+      });
+    });
+  });
 });
